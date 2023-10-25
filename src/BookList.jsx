411512681766abe0650e247ec71c3974312d46dc
FabioSimoDev <fabiofabietto182@gmail.com>
2023-10-25 17:25:12 +0200

    .

--- src/BookList.jsx
+++ src/BookList.jsx
@@ -1,13 +1,43 @@
 import SingleBook from "./SingleBook";
 import { Row } from "react-bootstrap";
+import React, { useState } from "react";
 
 const BookList = ({ books }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selected, setSelected] = useState(false);
+
+  const handleSearch = (e) => {
+    if (e.key === "Enter") {
+      setSearchTerm(e.target.value);
+    }
+  };
+
+  const handleBookClick = function (book) {
+    setSelected(book.asin === selected ? null : book.asin);
+  };
+
+  let filteredBooks = [];
+  if (searchTerm !== "") {
+    filteredBooks = books.filter((book) =>
+      book.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  } else {
+    filteredBooks.push(...books);
+  }
   return (
-    <Row xs={1} sm={2} md={4} xl={6} lg={5}>
-      {books.map((book) => (
-        <SingleBook book={book} />
-      ))}
-    </Row>
+    <div className="d-flex flex-column gap-3 align-items-center">
+      <input type="text" onKeyDown={handleSearch}></input>
+      <Row xs={1} sm={2} md={4} xl={6} lg={5}>
+        {filteredBooks.map((book) => (
+          <SingleBook
+            book={book}
+            key={book.asin}
+            isSelected={book.asin === selected}
+            onClick={() => handleBookClick(book)}
+          />
+        ))}
+      </Row>
+    </div>
   );
 };
 
--- src/SingleBook.jsx
+++ src/SingleBook.jsx
@@ -1,10 +1,11 @@
 import { Card, Col } from "react-bootstrap";
+import "./SingleBook.css";
 
-const SingleBook = ({ book }) => {
+const SingleBook = ({ book, isSelected, onClick }) => {
   return (
     // <Row xs={1} sm={2} md={4} xl={6} lg={5}>
-    <Col key={book.asin}>
-      <Card>
+    <Col>
+      <Card className={isSelected ? "selected-book" : ""} onClick={onClick}>
         <Card.Img variant="top" src={book.img} alt={`Cover of ${book.title}`} />
         <Card.Body>
           <Card.Title>{book.title}</Card.Title>
